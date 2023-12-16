3bfce56437c80d59422b3b6608b46ab6bae00e7a
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-12-16 17:39:06 +0200

    add createElement.tsx and createElement.css

---
+++ src/createElement.tsx
@@ -0,0 +1,12 @@
+import './createElement.css';
+function CreateElement(){
+  return (
+    <div>
+      <h2>To do list</h2>
+      <input type="text" value={'some text'}/>
+      <button>Add</button>
+
+    </div>
+  )
+}
+export default CreateElement;
